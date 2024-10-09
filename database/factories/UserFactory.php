e345cae6423a768eed4dee5111393277f48b6e5f
Polash Mahmud <polashmahmud@gmail.com>
2024-10-09 11:51:12 +0600

    add nid migration

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'nid' => fake()->unique()->numerify('#################'),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
