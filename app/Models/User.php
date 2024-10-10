0c11d0ee97a7c85838767511b3924be24b29dee9
Polash Mahmud <polashmahmud@gmail.com>
2024-10-10 15:53:17 +0600

    add tntsearch

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Scout\Searchable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, Searchable;
+
+    public $asYouType = true;
 
     /**
      * The attributes that are mass assignable.
